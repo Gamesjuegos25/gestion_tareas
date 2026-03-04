1cbb971a745d63b846302c1f05f1de60ed9f62a0
Mike656a <mreyesm28@miumg.edu.gt>
2026-03-04 13:12:26 -0600

    US-05.2
    se subio completo el repositorio para evitar conflicto con actualizaciones del develoment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // ¡Agrega esta línea para darle permiso a tu frontend!
+  app.enableCors();
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
